32d512976cd3ea214e789ff2c8dec9be8a9d3059
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-15 00:15:39 +0530

    like done

--- src/App.js
+++ src/App.js
@@ -3,30 +3,50 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>My Counter App</h1>
-      < CounterApp />
+      <h1>Counter App</h1>
+      <CounterApp />
+      <CounterApp />
+      <CounterApp />
+
     </>
   );
 }
 
 function CounterApp() {
-
   let [counter, setCounter] = useState(1);
 
-  let likeMeAction = () => {
+  let LikeMeAction = () => {
     counter = counter + 1;
-    //alert();
-    console.log("I am Button click", counter);
+    console.log(counter);
 
     setCounter(counter);
   };
+  
+  let [counter2, setCounter2] = useState(1);
+  let DisLikeMeAction = () => {
+    counter2 = counter2 - 1;
+    console.log(counter2);
 
+    setCounter(counter2);
+  };
   return (
     <>
-       <h1>{counter}</h1>
-      <input type="button" value="Like Me"
-      onClick={likeMeAction} />
+      <h5>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quia
+        consequatur, esse repellendus omnis hic mollitia aperiam neque, enim
+        aspernatur eum nobis asperiores harum, ducimus sunt ex. Ullam voluptatem
+      </h5>
+
+      <h5>
+        {counter}
+        <input type="button" value="&#128077;" onClick={LikeMeAction} />
+        {counter2}
+        <input type="button" value="&#128078;" onClick={DisLikeMeAction} />
+        
+      </h5>
+      
     </>
   );
 }
+
 export default App;
